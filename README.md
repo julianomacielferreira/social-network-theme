ce65ddace0754cf086797464f4ddbdc859a19bb2
Juliano Maciel Ferreira <ju.maciel.ferreira@gmail.com>
2019-09-22 08:19:37 -0300

    Wepack and static files created.

--- README.md
+++ README.md
@@ -2,4 +2,47 @@
 
 This is a website template based on the tutorial [Social Network Theme With Sass](https://www.youtube.com/playlist?list=PLillGF-Rfqba3xeEvDzIcUCxwMlGiewfV) from Traversy Media&trade;.
 
-@TODO - Define content, technologies, references, screenshots. 
+The icons are from the Font Awesome&trade; [https://fontawesome.com/](https://fontawesome.com/) library.
+
+# Disclaimer
+
+I coded this website template only for the purpose of learning. **All the credits go to Traversy Media&trade;.**
+
+- Traversy Media&trade; youtube channel: [https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA](https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA)
+
+- Traversy Media&trade; site: [https://www.traversymedia.com/](https://www.traversymedia.com/)
+
+- Font Awesome&trade; site: [https://fontawesome.com/](https://fontawesome.com/)
+
+- Sass site: [https://sass-lang.com/](https://sass-lang.com/)
+
+## How to Install and Run
+
+**1. Check if you have a recent version of [Node.js](https://nodejs.org/) (which comes bundled with [npm](https://www.npmjs.com/), a JavaScript package manager):**
+
+```bash
+$ node -v
+```
+
+```bash
+$ npm -v
+```
+
+**2. In the _project root directory_ install all the dependencies and libs:**
+
+```bash
+$ npm install
+```
+
+**3. Start the webpack-dev-server:**
+
+```bash
+$ npm run start
+```
+
+**4. Access [http://localhost:8080/](http://localhost:8080/) on your browser.**
+
+# License
+
+Please see the [license
+agreement](https://github.com/julianomacielferreira/social-network-theme/blob/master/LICENSE).
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = [{
+    entry: ['./scss/app.scss', './js/app.js'],
+    output: {
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundle.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                includePaths: ['./node_modules']
+                            }
+                        }
+                    },
+                ]
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{
+                    loader: 'file-loader'
+                }]
+            }
+        ]
+    },
+}];
